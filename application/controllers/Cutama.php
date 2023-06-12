0c7b5b312c32658ff6caa5eedbd6198d154535bc
Feek18 <bintangfikri31@gmail.com>
2023-06-12 11:56:33 +0800

    Update File

--- application/controllers/Cutama.php
+++ application/controllers/Cutama.php
@@ -1,7 +1,7 @@
 <?php
     class Cutama extends CI_Controller{
         function tampilview(){
-            $this->load->view('indexdashboard');
+            $this->load->view('tampilanawal');
         }
     }
     
